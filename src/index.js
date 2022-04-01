4f0d8e2e93e9a2e1ebb52669877104897eed0d0b
nat246 <37161194+nat246@users.noreply.github.com>
2022-04-01 19:01:44 +1000

    test

--- src/index.js
+++ src/index.js
@@ -20,12 +20,28 @@ function User(props) {
 
 /** Main */
 function App() {
-  const user = {};
+
+  /** This is a static user object */
+  const user = {
+    "data": {
+        "id": 2,
+        "email": "janet.weaver@reqres.in",
+        "first_name": "Janet",
+        "last_name": "Weaver",
+        "avatar": "https://reqres.in/img/faces/2-image.jpg"
+    },
+    "support": {
+        "url": "https://reqres.in/#support-heading",
+        "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+    }
+};
 
   return (
     <div className="App">
       <h1>User Details</h1>
-      <User />
+      
+      {/* Display the user's info from the static user object */}
+      <User {...user}/>
     </div>
   );
 }
